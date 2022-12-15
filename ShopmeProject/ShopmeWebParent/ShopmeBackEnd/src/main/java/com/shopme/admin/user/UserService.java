2ddd6db65757efbc560bfe1472d8cd18582a2972
HoangThong37 <thonghoang115@gmail.com>
2022-12-15 17:30:56 +0700

    task: encode password user

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -3,6 +3,7 @@ package com.shopme.admin.user;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.shopme.common.entity.Role;
@@ -16,6 +17,9 @@ public class UserService {
 
 	@Autowired
 	private RoleRepository roleRepository;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder; 
 
 	// find all user
 	public List<User> listAll() {
@@ -28,7 +32,14 @@ public class UserService {
 	}
 
 	public void save(User user) {
+		encodePassword(user);
 		userRepository.save(user);
+	}
+	
+	private void encodePassword(User user) {
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
+		
 	}
 
 }
