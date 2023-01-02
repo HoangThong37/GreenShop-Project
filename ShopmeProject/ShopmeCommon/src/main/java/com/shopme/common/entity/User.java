595c5a79d42b134bcb0afa37135439cc1a7603d4
HoangThong37 <thonghoang115@gmail.com>
2023-01-02 15:59:07 +0700

    task 16: code User Authentication

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -38,7 +39,7 @@ public class User {
 	
 	private boolean enabled; // kích hoạt
 	
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(
 			name = "user_roles",
 			joinColumns = @JoinColumn(name = "user_id"),
