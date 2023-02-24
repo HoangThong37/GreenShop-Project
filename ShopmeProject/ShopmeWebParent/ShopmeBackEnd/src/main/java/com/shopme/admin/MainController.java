88a93a2710bbd04a5d36d82fb0239fc87a38437c
HoangThong37 <thonghoang115@gmail.com>
2023-02-24 11:42:44 +0700

    task 58: code refactor

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MainController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MainController.java
@@ -1,5 +1,8 @@
 package com.shopme.admin;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -13,7 +16,10 @@ public class MainController {
 
 	@GetMapping("/login")
 	public String viewLoginPage() {
-		return "login";
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+			return "login";
+		}
+		return "redirect:/";
 	}
-	
 }
--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
@@ -1,24 +1,14 @@
 package com.shopme.admin.product;
 
-import static org.hamcrest.CoreMatchers.nullValue;
-
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,22 +16,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.lowagie.text.pdf.AcroFields.Item;
 import com.shopme.admin.FileUploadUntil;
 import com.shopme.admin.brand.BrandService;
-import com.shopme.admin.category.CategoryNotFoundException;
 import com.shopme.admin.category.CategoryService;
 import com.shopme.admin.security.ShopmeUserDetails;
 import com.shopme.common.entity.Brand;
 import com.shopme.common.entity.Category;
 import com.shopme.common.entity.Product;
-import com.shopme.common.entity.ProductImage;
 
 @Controller
 public class ProductController {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
-	
+
 	@Autowired
 	private ProductService serviceProduct;
 	
@@ -141,115 +126,21 @@ public class ProductController {
 				ra.addFlashAttribute("messageSuccess", "The product has been saved successfully.");
 				return "redirect:/products";
 			}
-		    setMainImageName(mainImageMultiparts,product);
-		    setExistingExtraImageName(imageIDs, imageNames, product); // set tên hình ảnh
-		    setNewExtraImageNames(extraImageMultiparts, product);
-		    setProductDetails(detailIDs,detailNames, detailValues, product);
+		    ProductSaveHelper.setMainImageName(mainImageMultiparts,product);
+		    ProductSaveHelper.setExistingExtraImageName(imageIDs, imageNames, product); // set tên hình ảnh
+		    ProductSaveHelper.setNewExtraImageNames(extraImageMultiparts, product);
+		    ProductSaveHelper.setProductDetails(detailIDs,detailNames, detailValues, product);
 	
 			Product savedProduct = serviceProduct.save(product);
-			saveUploadedImages(mainImageMultiparts, extraImageMultiparts, savedProduct);
+			ProductSaveHelper.saveUploadedImages(mainImageMultiparts, extraImageMultiparts, savedProduct);
 			
-			deleteExtraImagesWeredRemoveOnForm(product);
+			ProductSaveHelper.deleteExtraImagesWeredRemoveOnForm(product);
 			
 		ra.addFlashAttribute("messageSuccess", "The product has been saved successfully.");
 		return "redirect:/products";
 	} 
 	
-	private void deleteExtraImagesWeredRemoveOnForm(Product product) {
-		String extraImageDir = "../product-images/" + product.getId() + "/extras";
-		Path dirPath = Paths.get(extraImageDir);
-		
-		try {
-			Files.list(dirPath).forEach(file -> {
-				String fileName = file.toFile().getName();
-				
-				if(!product.containsImageName(fileName)) { // ko chứa
-					try {
-						Files.delete(file);
-						LOGGER.info("Deleted extra image : " + fileName);
-					} catch (IOException e) {
-						LOGGER.error("Could not delete extra image : " + fileName);
-					}
-				}
-			});
-		} catch (IOException e) {
-			LOGGER.error("Could not delete extra image : " + dirPath);
-		}
-		
-	}
-	private void setExistingExtraImageName(String[] imageIDs, String[] imageNames, Product product) {
-		if (imageIDs.length == 0 || imageIDs == null) return;
-		
-		Set<ProductImage> images = new HashSet<>();
-		for(int count = 0; count < imageIDs.length; count++) {
-			Integer id = Integer.parseInt(imageIDs[count]);
-			String name = imageNames[count];
-			images.add(new ProductImage(id, name, product));
-		}
-		product.setImages(images);
-	}
 	
-	private void setProductDetails(String[] detailIDs,
-			                       String[] detailNames,
-			                       String[] detailValues, Product product) {
-		if(detailNames == null || detailValues == null) { 
-			return;
-		}
-		for (int i = 0; i < detailNames.length; i++) {
-			String name = detailNames[i];
-			String value = detailValues[i];
-			Integer id  = Integer.parseInt(detailIDs[i]);
-			if (id!=0) {
-				product.addDetail(id, name, value);
-			} else if (name != null && value != null) {
-				product.addDetail(name, value);
-			}
-		}
-	}
-	private void saveUploadedImages(MultipartFile mainImageMultiparts,
-			                        MultipartFile[] extraImageMultiparts,
-			                        Product savedProduct) throws IOException {
-		if (!mainImageMultiparts.isEmpty()) {
-			String fileName = StringUtils.cleanPath(mainImageMultiparts.getOriginalFilename());
-			String uploadDir = "../product-images/" + savedProduct.getId();
-			FileUploadUntil.cleanDir(uploadDir);
-			FileUploadUntil.saveFile(uploadDir, fileName, mainImageMultiparts);
-		}
-		if (extraImageMultiparts.length > 0) {
-			String uploadDir = "../product-images/" + savedProduct.getId() + "/extras";
-			
-			for (MultipartFile multipartFile : extraImageMultiparts) {
-				if(multipartFile.isEmpty()) continue;
-				
-				String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
-				FileUploadUntil.saveFile(uploadDir, fileName, multipartFile);
-			}			
-		} 
-	}
-	
-	//set image in extra
-	private void setNewExtraImageNames(MultipartFile[] extraImageMultiparts,Product product) {
-		if (extraImageMultiparts.length > 0) {
-			for (MultipartFile multipartFile : extraImageMultiparts) {
-				if (!multipartFile.isEmpty()) {
-				String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
-				
-				if (!product.containsImageName(fileName)) {
-					product.addExtraImages(fileName);
-				}
-			}
-			}
-		}
-	}
-	
-	//set image in main
-	private void setMainImageName(MultipartFile mainImageMultiparts, Product product) {
-		if (!mainImageMultiparts.isEmpty()) {
-			String fileName = StringUtils.cleanPath(mainImageMultiparts.getOriginalFilename());
-
-			product.setMainImage(fileName);
-		}
-	}
 	
 	// code update product enabled
 	@GetMapping("/products/{id}/enabled/{status}")
